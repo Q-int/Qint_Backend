673eaf34b065de94e07014f0879f8abd798580d0
in-jun <injuninjune@gmail.com>
2024-07-15 22:20:27 +0900

    first commit

---
+++ src/main/java/org/example/qint_backend/QintBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.qint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QintBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QintBackendApplication.class, args);
+    }
+
+}
