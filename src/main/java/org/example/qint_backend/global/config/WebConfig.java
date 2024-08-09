ea1f82b9fa5b20d939af04d88334a6a4cdd9055c
in-jun <injuninjune@gmail.com>
2024-08-09 21:43:28 +0900

    fix: 코드 수정

--- src/main/java/org/example/qint_backend/global/config/WebConfig.java
+++ src/main/java/org/example/qint_backend/global/config/WebConfig.java
@@ -1,46 +1,29 @@
 package org.example.qint_backend.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.qint_backend.global.security.jwt.JwtTokenProvider;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
-@RequiredArgsConstructor
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
-public class WebSecurityConfig {
-    
-    private final JwtTokenProvider jwtTokenProvider;
-    private final ObjectMapper objectMapper;
+public class WebConfig {
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity httpSecurity, CorsConfigurationSource corsConfigurationSource) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
-                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(false);
 
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
 
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user/auth/**").permitAll()
-                        .requestMatchers("/invest/stocks").permitAll()
-                        .requestMatchers("/news/**").permitAll()
-                        .requestMatchers("/invest/stocks/**").permitAll()
-                        .anyRequest().authenticated())
-
-                .with(new FilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults());
-
-        return httpSecurity.build();
+        return source;
     }
 }
