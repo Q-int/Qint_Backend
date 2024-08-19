c013ed19bc80b376a709d900c56cc110e0aa514c
in-jun <injuninjune@gmail.com>
2024-08-19 22:48:06 +0900

    feat: EmailVerification 엔티티 추가

---
+++ src/main/java/org/example/qint_backend/domain/email/domain/EmailVerification.java
@@ -0,0 +1,44 @@
+package org.example.qint_backend.domain.email.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class EmailVerification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String authCode;
+
+    @Column(nullable = false)
+    private Boolean isVerified = false;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public EmailVerification(String email, String authCode) {
+        this.email = email;
+        this.authCode = authCode;
+        this.isVerified = false;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void updateAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public void verify() {
+        this.isVerified = true;
+    }
+}
