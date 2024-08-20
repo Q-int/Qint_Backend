2375ea20f6f4e2d766e7c30fa5d62ed2af47f251
in-jun <injuninjune@gmail.com>
2024-08-20 19:25:13 +0900

    feat: EmailVerificationNotFoundException 사용자 정의 예외 추가

---
+++ src/main/java/org/example/qint_backend/domain/email/exception/EmailVerificationNotFoundException.java
@@ -0,0 +1,13 @@
+package org.example.qint_backend.domain.email.exception;
+
+import org.example.qint_backend.global.err.exception.ErrorCode;
+import org.example.qint_backend.global.err.exception.QintException;
+
+public class EmailVerificationNotFoundException extends QintException {
+
+    public static QintException EXCEPTION = new EmailVerificationNotFoundException();
+
+    private EmailVerificationNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}
