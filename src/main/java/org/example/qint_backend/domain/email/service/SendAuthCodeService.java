ed46f7f6fe2860ceb2276f69fae167a8af9c5c49
in-jun <injuninjune@gmail.com>
2024-08-20 19:46:24 +0900

    feat: 이메일로 인증코드를 보내는 service 추가

---
+++ src/main/java/org/example/qint_backend/domain/email/service/SendAuthCodeService.java
@@ -0,0 +1,41 @@
+package org.example.qint_backend.domain.email.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.qint_backend.domain.email.domain.EmailVerification;
+import org.example.qint_backend.domain.email.domain.repository.EmailVerificationRepository;
+import org.example.qint_backend.domain.email.facade.EmailVerificationFacade;
+import org.example.qint_backend.domain.email.presentation.dto.request.SendAuthCodeRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class SendAuthCodeService {
+
+    private final EmailService emailService;
+    private final EmailVerificationRepository emailVerificationRepository;
+    private final EmailVerificationFacade emailVerificationFacade;
+
+    @Transactional
+    public void execute(SendAuthCodeRequest sendAuthCodeRequest) {
+        String email = sendAuthCodeRequest.getEmail();
+        String authCode = generateAuthCode();
+
+        if (emailVerificationRepository.existsByEmail(email)) {
+            EmailVerification emailVerification = emailVerificationFacade.getEmailVerificationByEmail(email);
+            emailVerification.updateAuthCode(authCode);
+        } else {
+            emailVerificationRepository.save(new EmailVerification(email, authCode));
+        }
+
+        emailService.sendEmail(email, authCode);
+    }
+
+    private String generateAuthCode() {
+        Random random = new Random();
+        int code = 100000 + random.nextInt(900000);
+        return String.valueOf(code);
+    }
+}
