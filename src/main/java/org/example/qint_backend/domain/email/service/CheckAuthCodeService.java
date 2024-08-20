1ff9fca3c8cdc46c441830e69ac10596eee5353d
in-jun <injuninjune@gmail.com>
2024-08-20 19:48:04 +0900

    feat: 인증코드를 확인하는 service 추가

---
+++ src/main/java/org/example/qint_backend/domain/email/service/CheckAuthCodeService.java
@@ -0,0 +1,27 @@
+package org.example.qint_backend.domain.email.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.qint_backend.domain.email.domain.EmailVerification;
+import org.example.qint_backend.domain.email.facade.EmailVerificationFacade;
+import org.example.qint_backend.domain.email.presentation.dto.request.CheckAuthCodeRequest;
+import org.example.qint_backend.domain.email.presentation.dto.response.CheckAuthCodeResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CheckAuthCodeService {
+    private final EmailVerificationFacade emailVerificationFacade;
+
+    @Transactional
+    public CheckAuthCodeResponse execute(CheckAuthCodeRequest checkAuthCodeRequest) {
+        EmailVerification emailVerification = emailVerificationFacade.getEmailVerificationByEmail(checkAuthCodeRequest.getEmail());
+
+        if (emailVerification.getAuthCode().equals(checkAuthCodeRequest.getAuthCode())) {
+            emailVerification.verify();
+            return CheckAuthCodeResponse.builder().isVerified(true).build();
+        }
+
+        return CheckAuthCodeResponse.builder().isVerified(false).build();
+    }
+}
