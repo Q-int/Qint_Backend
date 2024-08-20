b5993cffa2078c64d552b1cf7ee299b1a27c1d75
in-jun <injuninjune@gmail.com>
2024-08-20 19:14:51 +0900

    feat: 이메일 중복 체크를 위한 existsByEmail메서드 추가

--- src/main/java/org/example/qint_backend/domain/email/domain/repository/EmailVerificationRepository.java
+++ src/main/java/org/example/qint_backend/domain/email/domain/repository/EmailVerificationRepository.java
@@ -3,5 +3,10 @@ package org.example.qint_backend.domain.email.domain.repository;
 import org.example.qint_backend.domain.email.domain.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+    Optional<EmailVerification> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
