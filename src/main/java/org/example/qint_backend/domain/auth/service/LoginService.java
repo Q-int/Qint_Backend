7f7999ab4366541750e62d1c76082e4167c5719b
in-jun <injuninjune@gmail.com>
2024-08-21 16:51:07 +0900

    feat: 로그인 Service 추가

---
+++ src/main/java/org/example/qint_backend/domain/auth/service/LoginService.java
@@ -0,0 +1,34 @@
+package org.example.qint_backend.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.qint_backend.domain.auth.exception.PasswordNotValidException;
+import org.example.qint_backend.domain.auth.presentation.dto.request.LoginRequest;
+import org.example.qint_backend.domain.auth.presentation.dto.response.TokenResponse;
+import org.example.qint_backend.domain.user.domain.User;
+import org.example.qint_backend.domain.user.facade.UserFacade;
+import org.example.qint_backend.global.security.jwt.JwtTokenProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserFacade userFacade;
+
+    public TokenResponse execute(LoginRequest loginRequest) {
+        User user = userFacade.getUserByEmail(loginRequest.getEmail());
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw PasswordNotValidException.EXCEPTION;
+        }
+
+        return jwtTokenProvider.generateToken(user.getEmail(), "USER");
+    }
+
+
+}
