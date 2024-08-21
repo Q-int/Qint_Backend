556f368debbdb70857ad0985e9e783c7a49fb118
in-jun <injuninjune@gmail.com>
2024-08-21 12:33:13 +0900

    feat getCurrentUser 메서드 추가

--- src/main/java/org/example/qint_backend/domain/user/facade/UserFacade.java
+++ src/main/java/org/example/qint_backend/domain/user/facade/UserFacade.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.user.domain.User;
 import org.example.qint_backend.domain.user.domain.repository.UserRepository;
 import org.example.qint_backend.domain.user.exception.UserNotFoundException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,11 @@ import org.springframework.stereotype.Component;
 public class UserFacade {
 
     private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByEmail(email);
+    }
 
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(() -> UserNotFoundException.EXCEPTION);
