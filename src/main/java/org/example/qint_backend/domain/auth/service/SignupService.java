a50b5bcafe22c67f3b86452760bf1d0d0debcf1f
in-jun <injuninjune@gmail.com>
2024-08-21 16:43:17 +0900

    feat: 회원가입 Service 추가

---
+++ src/main/java/org/example/qint_backend/domain/auth/service/SignupService.java
@@ -0,0 +1,44 @@
+package org.example.qint_backend.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.qint_backend.domain.auth.presentation.dto.request.SignupRequest;
+import org.example.qint_backend.domain.email.exception.EmailNotVerifiedException;
+import org.example.qint_backend.domain.email.facade.EmailVerificationFacade;
+import org.example.qint_backend.domain.user.domain.User;
+import org.example.qint_backend.domain.user.domain.repository.UserRepository;
+import org.example.qint_backend.domain.user.exception.UserAlreadyExistsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SignupService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    private final EmailVerificationFacade emailVerificationFacade;
+
+    public void execute(SignupRequest signupRequest) {
+        String email = signupRequest.getEmail();
+        String password = passwordEncoder.encode(signupRequest.getPassword());
+
+        if (userRepository.existsByEmail(email)) {
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+
+        if (!emailVerificationFacade.getEmailVerificationByEmail(email).getIsVerified()) {
+            throw EmailNotVerifiedException.EXCEPTION;
+        }
+
+        User user = User.builder()
+                .email(email)
+                .password(password)
+                .correctAnswers(0L)
+                .incorrectAnswer(0L)
+                .build();
+
+        userRepository.save(user);
+    }
+}
