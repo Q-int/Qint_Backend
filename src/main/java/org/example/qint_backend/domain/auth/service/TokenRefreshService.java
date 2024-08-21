8bd8982cbd261a87bf992cdbfe3fcc34286db6d9
in-jun <injuninjune@gmail.com>
2024-08-21 16:51:48 +0900

    feat: 토큰 재발급 Service 추가

---
+++ src/main/java/org/example/qint_backend/domain/auth/service/TokenRefreshService.java
@@ -0,0 +1,16 @@
+package org.example.qint_backend.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.qint_backend.domain.auth.presentation.dto.response.TokenResponse;
+import org.example.qint_backend.global.security.jwt.TokenRefreshUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenRefreshService {
+    private final TokenRefreshUtil tokenRefreshUtil;
+
+    public TokenResponse execute(String refreshToken) {
+        return tokenRefreshUtil.tokenRefresh(refreshToken);
+    }
+}
