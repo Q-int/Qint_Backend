db10d6d3a88dc12a511cef90d8bc37f890635d2e
in-jun <injuninjune@gmail.com>
2024-08-22 11:38:26 +0900

    refactor: 코드 형식 수정

--- src/main/java/org/example/qint_backend/domain/email/service/CheckAuthCodeService.java
+++ src/main/java/org/example/qint_backend/domain/email/service/CheckAuthCodeService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class CheckAuthCodeService {
+
     private final EmailVerificationFacade emailVerificationFacade;
 
     @Transactional
