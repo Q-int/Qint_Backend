2a62e2a066a8990786263972c8f3b826ee9be8ff
in-jun <injuninjune@gmail.com>
2024-08-23 20:00:38 +0900

    feat: 오타 수정 password -> email

--- src/main/java/org/example/qint_backend/domain/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/org/example/qint_backend/domain/auth/presentation/dto/request/LoginRequest.java
@@ -2,20 +2,15 @@ package org.example.qint_backend.domain.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
 
-    @NotBlank(message = "password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
+    @NotBlank(message = "공백 또는 띄어쓰기를 허용하지 않습니다.")
     @Email(message = "email 형식")
     private String email;
 
     @NotBlank(message = "password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(
-            regexp =
-                    "^(?=.*[!@#$%^&*])(?=.{1,20}$).*",
-            message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다.")
     private String password;
 }
