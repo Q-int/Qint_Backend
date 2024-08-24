93403346e465a2cb85feb67604f83a6de321af12
in-jun <injuninjune@gmail.com>
2024-08-24 19:04:53 +0900

    feat: EmailVerificationNotFoundException를 EmailNotVerifiedException로 통합

--- src/main/java/org/example/qint_backend/domain/email/facade/EmailVerificationFacade.java
+++ src/main/java/org/example/qint_backend/domain/email/facade/EmailVerificationFacade.java
@@ -3,7 +3,7 @@ package org.example.qint_backend.domain.email.facade;
 import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.email.domain.EmailVerification;
 import org.example.qint_backend.domain.email.domain.repository.EmailVerificationRepository;
-import org.example.qint_backend.domain.email.exception.EmailVerificationNotFoundException;
+import org.example.qint_backend.domain.email.exception.EmailNotVerifiedException;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -13,6 +13,6 @@ public class EmailVerificationFacade {
     private final EmailVerificationRepository emailVerificationRepository;
 
     public EmailVerification getEmailVerificationByEmail(String email) {
-        return emailVerificationRepository.findByEmail(email).orElseThrow(() -> EmailVerificationNotFoundException.EXCEPTION);
+        return emailVerificationRepository.findByEmail(email).orElseThrow(() -> EmailNotVerifiedException.EXCEPTION);
     }
 }
