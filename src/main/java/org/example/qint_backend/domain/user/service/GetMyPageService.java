24c1a2e59a4fd80765f27f0dcc2f90d6e583155d
coehgns <modooboiroo@gmail.com>
2024-08-25 22:02:58 +0900

    refactor: MyPageService를 GetMyPageService로 변경

--- src/main/java/org/example/qint_backend/domain/user/service/GetMyPageService.java
+++ src/main/java/org/example/qint_backend/domain/user/service/GetMyPageService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MyPageService {
+public class GetMyPageService {
 
     private final UserFacade userFacade;
 
