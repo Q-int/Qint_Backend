df1e7ddf034397375e1b498bb9ad99a0d6a16a8c
coehgns <modooboiroo@gmail.com>
2024-08-25 22:02:33 +0900

    refactor: IncorrectProblemsService를 GetIncorrectProblemsService로 변경

--- src/main/java/org/example/qint_backend/domain/user/service/GetIncorrectProblemsService.java
+++ src/main/java/org/example/qint_backend/domain/user/service/GetIncorrectProblemsService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class IncorrectProblemsService {
+public class GetIncorrectProblemsService {
 
     private final UserFacade userFacade;
 
