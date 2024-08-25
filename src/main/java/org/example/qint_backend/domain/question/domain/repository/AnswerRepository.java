8da1a7a4d8a30803858a449af335e2cb59033f08
coehgns <modooboiroo@gmail.com>
2024-08-25 22:11:58 +0900

    refactor: findAllByQuestionAndAndIsCorrectIsTrue를 findAllByQuestionAndIsCorrectIsTrue로 수정

--- src/main/java/org/example/qint_backend/domain/question/domain/repository/AnswerRepository.java
+++ src/main/java/org/example/qint_backend/domain/question/domain/repository/AnswerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    Optional<Answer> findAllByQuestionAndAndIsCorrectIsTrue(Question question);
+    Optional<Answer> findAllByQuestionAndIsCorrectIsTrue(Question question);
 }
