93d3050e1112b053713f0d2519082c53575f3887
coehgns <modooboiroo@gmail.com>
2024-08-26 21:48:47 +0900

    refactor: 반환 값 변경
    List<UserIncorrectAnswerResponse> -> UserIncorrectAnswerResponse

--- src/main/java/org/example/qint_backend/domain/user/presentation/UserController.java
+++ src/main/java/org/example/qint_backend/domain/user/presentation/UserController.java
@@ -1,5 +1,6 @@
 package org.example.qint_backend.domain.user.presentation;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.user.presentation.dto.response.UserAnswersResponse;
 import org.example.qint_backend.domain.user.presentation.dto.response.UserIncorrectAnswerResponse;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -26,7 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/incorrect-problems")
-    public List<UserIncorrectAnswerResponse> selectIncorrectAnswers() {
+    public UserIncorrectAnswerResponse selectIncorrectAnswers() {
         return getIncorrectProblemsService.execute();
     }
 
