a563df27285466d5f7e29d258daa6d6ad5f57f42
coehgns <modooboiroo@gmail.com>
2024-08-26 21:45:46 +0900

    chore: import문 수정

--- src/main/java/org/example/qint_backend/domain/user/service/GetMyPageService.java
+++ src/main/java/org/example/qint_backend/domain/user/service/GetMyPageService.java
@@ -3,7 +3,6 @@ package org.example.qint_backend.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.user.presentation.dto.response.UserAnswersResponse;
 import org.example.qint_backend.domain.user.domain.User;
-import org.example.qint_backend.domain.user.domain.repository.UserIncorrectAnswersRepository;
 import org.example.qint_backend.domain.user.facade.UserFacade;
 import org.springframework.stereotype.Service;
 
