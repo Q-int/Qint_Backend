9a78eab0de14af37b0ec1bcf8e793a4bdc6f1039
coehgns <modooboiroo@gmail.com>
2024-08-26 09:09:50 +0900

    refactor: List를 DTO로 한 번 감싸는 DTO로 변경

--- src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserIncorrectAnswerResponse.java
+++ src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserIncorrectAnswerResponse.java
@@ -3,16 +3,10 @@ package org.example.qint_backend.domain.user.presentation.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class UserIncorrectAnswerResponse {
-
-    private String contents;
-
-    private String commentary;
-
-    private String incorrectAnswer;
-
-    private String correctAnswer;
-
+    List<IncorrectAnswerResponse> incorrectAnswerResponseList;
 }
