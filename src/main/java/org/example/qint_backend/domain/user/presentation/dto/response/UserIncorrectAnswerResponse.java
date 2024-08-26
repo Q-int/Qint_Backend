434cf3b25897bc68f223dc5e00e860f334f2f367
coehgns <modooboiroo@gmail.com>
2024-08-26 20:20:47 +0900

    refactor: 코드 형식 수정

--- src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserIncorrectAnswerResponse.java
+++ src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserIncorrectAnswerResponse.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class UserIncorrectAnswerResponse {
-    List<UserIncorrectAnswerElement> userIncorrectAnswerList;
+    private final List<UserIncorrectAnswerElement> userIncorrectAnswerElementList;
 }
