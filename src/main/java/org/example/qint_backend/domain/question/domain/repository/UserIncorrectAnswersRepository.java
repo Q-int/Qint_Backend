1c664e8b58f114eea213effb47cb899ff98fc33b
coehgns <modooboiroo@gmail.com>
2024-08-26 09:11:44 +0900

    refactor: 반환 값 변경 List -> DTO

--- src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
+++ src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
@@ -2,11 +2,13 @@ package org.example.qint_backend.domain.question.domain.repository;
 
 import org.example.qint_backend.domain.question.domain.UserIncorrectAnswers;
 import org.example.qint_backend.domain.user.domain.User;
+import org.example.qint_backend.domain.user.presentation.dto.response.IncorrectAnswerResponse;
+import org.example.qint_backend.domain.user.presentation.dto.response.UserIncorrectAnswerResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface UserIncorrectAnswersRepository extends JpaRepository<UserIncorrectAnswers, Long> {
 
-    List<UserIncorrectAnswers> findAllByUser(User user);
+    UserIncorrectAnswerResponse findAllByUser(User user);
 }
