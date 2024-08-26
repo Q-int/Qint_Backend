03a47179595cbe0b7dbc8a6c925b24641cf2b1f9
coehgns <modooboiroo@gmail.com>
2024-08-26 20:29:15 +0900

    feat: @Repository 추가

--- src/main/java/org/example/qint_backend/domain/user/domain/repository/UserIncorrectAnswersRepository.java
+++ src/main/java/org/example/qint_backend/domain/user/domain/repository/UserIncorrectAnswersRepository.java
@@ -1,7 +1,13 @@
 package org.example.qint_backend.domain.user.domain.repository;
 
 import org.example.qint_backend.domain.question.domain.UserIncorrectAnswers;
+import org.example.qint_backend.domain.user.domain.User;
+import org.example.qint_backend.domain.user.presentation.dto.response.UserIncorrectAnswerElement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserIncorrectAnswersRepository extends JpaRepository<UserIncorrectAnswers, Long> {
 }
