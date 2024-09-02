fb0a0f70cb5d908a36e9ef285652f2af96b4c305
최선재 <1210thatman@gmail.com>
2024-09-02 22:34:59 +0900

    chore: UserIncorrectRepository 오탈자 수정

--- src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
+++ src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
@@ -1,8 +1,9 @@
 package org.example.qint_backend.domain.question.domain.repository;
 
+import org.example.qint_backend.domain.question.domain.UserIncorrectAnswers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserIncorrectAnswersRepository extends JpaRepository<UserIncorrectAnswersRepository, Long> {
+public interface UserIncorrectAnswersRepository extends JpaRepository<UserIncorrectAnswers, Long> {
 }
