9568af5fe95a24d8307c20d285a041c48703fe9f
coehgns <modooboiroo@gmail.com>
2024-09-03 21:42:36 +0900

    feat: OptionsElement 추가

---
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/OptionsElement.java
@@ -0,0 +1,11 @@
+package org.example.qint_backend.domain.question.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class OptionsElement {
+   private Long answerId;
+   private String text;
+}
