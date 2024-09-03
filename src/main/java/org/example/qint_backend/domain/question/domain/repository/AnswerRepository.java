6779f3741b63351ac681e6186ff2e97c18d0929b
coehgns <modooboiroo@gmail.com>
2024-09-04 08:08:48 +0900

    feat: findAllByQuestion 추가

--- src/main/java/org/example/qint_backend/domain/question/domain/repository/AnswerRepository.java
+++ src/main/java/org/example/qint_backend/domain/question/domain/repository/AnswerRepository.java
@@ -5,9 +5,12 @@ import org.example.qint_backend.domain.question.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    List<Answer> findAllByQuestion(Question question);
     Optional<Answer> findAllByQuestionAndIsCorrectIsTrue(Question question);
 }
