457d9b9121d39203586b34d469c3b96c3b99e5e0
coehgns <modooboiroo@gmail.com>
2024-09-03 21:42:53 +0900

    feat: IncorrectQuestionElement 추가

---
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/IncorrectQuestionElement.java
@@ -0,0 +1,14 @@
+package org.example.qint_backend.domain.question.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class IncorrectQuestionElement {
+    private Long question_Id;
+    private String contents;
+    private List<OptionsElement> options;
+}
