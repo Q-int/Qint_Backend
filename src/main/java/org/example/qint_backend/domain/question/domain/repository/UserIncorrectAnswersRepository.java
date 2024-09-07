7eabd4d624533cab3624cc0a6a56033d585d7d63
coehgns <modooboiroo@gmail.com>
2024-09-07 21:18:56 +0900

    fix: enum 값을 찾을 때 생기는 버그 수정
    findAllByQuestionCategoryName -> findAllByQuestionCategory

--- src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
+++ src/main/java/org/example/qint_backend/domain/question/domain/repository/UserIncorrectAnswersRepository.java
@@ -1,5 +1,6 @@
 package org.example.qint_backend.domain.question.domain.repository;
 
+import org.example.qint_backend.domain.question.domain.Category;
 import org.example.qint_backend.domain.question.domain.UserIncorrectAnswers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import java.util.List;
 @Repository
 public interface UserIncorrectAnswersRepository extends JpaRepository<UserIncorrectAnswers, Long> {
     List<UserIncorrectAnswers> findAllByUser(User user);
-    List<UserIncorrectAnswers> findAllByQuestionCategoryName(String category);
+    List<UserIncorrectAnswers> findAllByQuestionCategory(Category category);
 }
