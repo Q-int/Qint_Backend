a4623045c4934c36927bb8c0096757e6c46291e4
coehgns <modooboiroo@gmail.com>
2024-09-08 22:56:35 +0900

    refactor: @RequestParam -> @RequestBody 수정

--- src/main/java/org/example/qint_backend/domain/question/presentation/QuestionController.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/QuestionController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.question.presentation.dto.request.CategoryRequest;
 import org.example.qint_backend.domain.question.presentation.dto.response.QuestionByCategoryResponse;
 import org.example.qint_backend.domain.question.service.GetQuestionByCategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
     private final GetQuestionByCategoryService getQuestionByCategoryService;
-
     @GetMapping
     public QuestionByCategoryResponse getQuestionsByCategory(
-            @RequestParam(name = "categorys") CategoryRequest request
+            @RequestBody CategoryRequest request
     ) {
         return getQuestionByCategoryService.execute(request);
     }
