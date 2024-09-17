499f4b08d8e84c94f5527923f9891322093b8ff3
coehgns <modooboiroo@gmail.com>
2024-09-17 16:27:33 +0900

    refactor: path 변경 problems -> questions

--- src/main/java/org/example/qint_backend/domain/question/presentation/QuestionController.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/problems")
+@RequestMapping("/questions")
 public class QuestionController {
 
     private final GetQuestionByCategoryService getQuestionByCategoryService;
