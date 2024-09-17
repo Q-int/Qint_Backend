cc8665757c52c0b8367f8b4a60ff928b046b1e0c
coehgns <modooboiroo@gmail.com>
2024-09-17 16:31:31 +0900

    chore: @Builder 제거

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/QuestionByCategoryElement.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/QuestionByCategoryElement.java
@@ -1,12 +1,10 @@
 package org.example.qint_backend.domain.question.presentation.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@Builder
 public class QuestionByCategoryElement {
     private Long questionId;
     private String contents;
--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/QuestionByCategoryResponse.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/QuestionByCategoryResponse.java
@@ -1,12 +1,10 @@
 package org.example.qint_backend.domain.question.presentation.dto.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@Builder
 public class QuestionByCategoryResponse {
     List<QuestionByCategoryElement> questions;
 
