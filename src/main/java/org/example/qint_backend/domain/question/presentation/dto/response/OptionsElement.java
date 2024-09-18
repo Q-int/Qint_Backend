809953c89ae5748aa9922d4cab78b51b3a2eb69d
coehgns <modooboiroo@gmail.com>
2024-09-18 21:14:33 +0900

    feat: static 팩토리 메소드 추가

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/OptionsElement.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/OptionsElement.java
@@ -1,6 +1,7 @@
 package org.example.qint_backend.domain.question.presentation.dto.response;
 
 import lombok.Getter;
+import org.example.qint_backend.domain.question.domain.Answer;
 
 @Getter
 public class OptionsElement {
@@ -10,5 +11,9 @@ public class OptionsElement {
    public OptionsElement(Long answerId, String text) {
       this.answerId = answerId;
       this.text = text;
+   }
+
+   public static OptionsElement of(Answer answer) {
+      return new OptionsElement(answer.getId(), answer.getText());
    }
 }
