ae395664228afb4443b437e358a717f1c1a961e4
최선재 <1210thatman@gmail.com>
2024-10-29 14:41:59 +0900

    feat: ErrorCode에 errorCode 추가

---
+++ src/main/java/org/example/qint_backend/domain/question/exception/InvalidQuestionIdException.java
@@ -0,0 +1,13 @@
+package org.example.qint_backend.domain.question.exception;
+
+import org.example.qint_backend.global.err.exception.ErrorCode;
+import org.example.qint_backend.global.err.exception.QintException;
+
+public class InvalidQuestionIdException extends QintException {
+
+    public static final QintException EXCEPTION = new InvalidQuestionIdException();
+
+    private InvalidQuestionIdException() {
+        super(ErrorCode.INVALID_QUESTION_ID_EXCEPTION);
+    }
+}
