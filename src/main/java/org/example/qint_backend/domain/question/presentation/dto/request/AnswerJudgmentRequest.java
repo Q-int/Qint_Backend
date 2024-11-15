3e500f21f823f95cfff5406e7cb1c55d59bf9167
coehgns <modooboiroo@gmail.com>
2024-11-15 16:04:45 +0900

    feat: @JsonProperty

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/AnswerJudgmentRequest.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/AnswerJudgmentRequest.java
@@ -1,10 +1,14 @@
 package org.example.qint_backend.domain.question.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class AnswerJudgmentRequest {
 
+    @JsonProperty("question_id")
     private Long questionId;
+
+    @JsonProperty("answer_id")
     private Long answerId;
 }
--- src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserAnswersResponse.java
+++ src/main/java/org/example/qint_backend/domain/user/presentation/dto/response/UserAnswersResponse.java
@@ -1,5 +1,6 @@
 package org.example.qint_backend.domain.user.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,7 +8,9 @@ import lombok.Getter;
 @Builder
 public class UserAnswersResponse {
 
+    @JsonProperty("correct_answers")
     private Long correctAnswers;
 
+    @JsonProperty("incorrect_answers")
     private Long incorrectAnswers;
 }
