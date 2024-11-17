519e43fc16bd5f8b40a82bf6652ddc13c57714dd
coehgns <modooboiroo@gmail.com>
2024-11-17 19:23:13 +0900

    refactor: 변수명 수정

--- src/main/java/org/example/qint_backend/domain/question/service/MoveToNextProblemService.java
+++ src/main/java/org/example/qint_backend/domain/question/service/MoveToNextProblemService.java
@@ -15,7 +15,7 @@ public class MoveToNextProblemService {
     private final UserFacade userFacade;
     private final UserRepository userRepository;
 
-    private static final int MAX_ANSWER_COUNT = 15;
+    private static final int MAX_QUESTIONS_COUNTS = 15;
 
     @Transactional
     public void execute(MoveToNextProblemRequest request) {
@@ -27,7 +27,7 @@ public class MoveToNextProblemService {
 
         long sumAnswers = correctAnswers + incorrectAnswers;
 
-        if (sumAnswers == MAX_ANSWER_COUNT) {
+        if (sumAnswers == MAX_QUESTIONS_COUNTS) {
             user.resetAnswersCounts(correctAnswers, incorrectAnswers);
         }
 
