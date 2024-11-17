a817f65df489017168c17083d6ca5e6db495e388
coehgns <modooboiroo@gmail.com>
2024-11-17 15:14:10 +0900

    feat: @JsonProperty
    MoveToHomeRequest에 @JsonProperty 어노테이션 추가

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/MoveToHomeRequest.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/MoveToHomeRequest.java
@@ -1,8 +1,10 @@
 package org.example.qint_backend.domain.question.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class MoveToHomeRequest {
+    @JsonProperty("move_to_home")
     private boolean moveToHome;
 }
