eef6e3c7df444d067087d5d80b60ec239017e70a
coehgns <modooboiroo@gmail.com>
2024-11-17 20:06:42 +0900

    feat: @Builder

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/MoveToHomeResponse.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/response/MoveToHomeResponse.java
@@ -1,9 +1,11 @@
 package org.example.qint_backend.domain.question.presentation.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class MoveToHomeResponse {
 
     @JsonProperty("correct_answers")
