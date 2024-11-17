be1320238ff2c10dfa975be9f274e2a48f212b7f
도훈 <modooboiroo@gmail.com>
2024-11-17 20:43:09 +0900

    fix: 스네이크 케이스로 요청이 안되는 버그 수정

--- src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/MoveToNextProblemRequest.java
+++ src/main/java/org/example/qint_backend/domain/question/presentation/dto/request/MoveToNextProblemRequest.java
@@ -1,10 +1,8 @@
 package org.example.qint_backend.domain.question.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class MoveToNextProblemRequest {
-    @JsonProperty("move_to_next_problem")
     private boolean moveToNextProblem;
 }
