c5a5e5c66e2156afcd44d50441e49a826745170a
coehgns <modooboiroo@gmail.com>
2024-11-17 19:17:38 +0900

    feat: saveUserIncorrectAnswer

--- src/main/java/org/example/qint_backend/domain/question/service/MoveToNextProblemService.java
+++ src/main/java/org/example/qint_backend/domain/question/service/MoveToNextProblemService.java
@@ -3,6 +3,7 @@ package org.example.qint_backend.domain.question.service;
 import lombok.RequiredArgsConstructor;
 import org.example.qint_backend.domain.question.presentation.dto.request.MoveToNextProblemRequest;
 import org.example.qint_backend.domain.user.domain.User;
+import org.example.qint_backend.domain.user.domain.repository.UserRepository;
 import org.example.qint_backend.domain.user.facade.UserFacade;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,15 +13,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class MoveToNextProblemService {
 
     private final UserFacade userFacade;
+    private final UserRepository userRepository;
+
+    private static final int MAX_ANSWER_COUNT = 15;
 
     @Transactional
     public void execute(MoveToNextProblemRequest request) {
         User user = userFacade.getCurrentUser();
         boolean moveToNextProblem = request.isMoveToNextProblem();
-        Long incorrectAnswers = user.getIncorrectAnswers() + 1;
 
-        if(!moveToNextProblem) {
-            user.updateIncorrectAnswersCounts(incorrectAnswers);
+        Long correctAnswers = user.getCorrectAnswers();
+        Long incorrectAnswers = user.getIncorrectAnswers();
+
+        long sumAnswers = correctAnswers + incorrectAnswers;
+
+        if (sumAnswers == MAX_ANSWER_COUNT) {
+            user.resetAnswersCounts(correctAnswers, incorrectAnswers);
         }
+
+        if (!moveToNextProblem) {
+            saveUserIncorrectAnswer();
+        }
+    }
+
+    private void saveUserIncorrectAnswer() {
+        User user = userFacade.getCurrentUser();
+        Long incorrectAnswers = user.getIncorrectAnswers();
+        user.updateIncorrectAnswersCounts(incorrectAnswers + 1);
+        userRepository.save(user);
     }
 }
